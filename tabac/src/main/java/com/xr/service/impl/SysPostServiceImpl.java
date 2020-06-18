6dc82a66a1b2c71335a5956df8489e9065435dd3
Jmylsin <3526653946@qq.com>
2020-06-18 11:17:09 +0800

    SysPostservice 实现了service接口

--- tabac/src/main/java/com/xr/service/impl/SysPostServiceImpl.java
+++ tabac/src/main/java/com/xr/service/impl/SysPostServiceImpl.java
@@ -1,9 +1,38 @@
 package com.xr.service.impl;
 
+import com.xr.model.SysJob;
+import com.xr.service.SysPostService;
+
+import java.util.List;
+
 /**
  * @author Zephyr.Liu
  * @date 2020/6/18 3:01
  * @Description
  */
-public class SysPostServiceImpl {
+public class SysPostServiceImpl implements SysPostService {
+    @Override
+    public List<SysJob> selectByJob(SysJob sysJob) {
+        return null;
+    }
+
+    @Override
+    public void addJob(SysJob sysJob) {
+
+    }
+
+    @Override
+    public void updateJob() {
+
+    }
+
+    @Override
+    public void deleteJobById(int id) {
+
+    }
+
+    @Override
+    public List<SysJob> selectAllJobs(SysJob sysJob) {
+        return null;
+    }
 }
